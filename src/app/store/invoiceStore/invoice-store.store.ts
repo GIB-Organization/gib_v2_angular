f2ab8d0f95086e1eade13653abe7d4dbfb7dc419
wasemgamal <engwasem20@gmail.com>
2024-10-17 14:30:55 +0300

    fix/ checkout payment card rtl to ltr

--- src/app/store/invoiceStore/invoice-store.store.ts
+++ src/app/store/invoiceStore/invoice-store.store.ts
@@ -4,17 +4,7 @@ import { IInvoiceStore } from './invoice-store.interface';
 
 const initValue = () : IInvoiceStore =>{
   return {
-    invoices:[
-      {
-        id: 'sdfsdfs432342',
-        totalPrice: '1200',
-        detailsAr: 'شراء تأمين من شركه الخليج',
-        detailsEn: 'Buying Insurance from gulf company',
-        invoiceDate: 'Mon, 07 Oct 2024 12:07:39 GMT',
-        invoiceUrl: '1',
-        email: 'engwasem20@gmail.com'
-      }
-    ]
+    invoices:[]
   }
 }
 @Injectable({
